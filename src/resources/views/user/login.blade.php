dab2802cee37d1c0ebeee8d1081df3558a2b213c
Rizaldi <rizaldihprastomo@gmail.com>
2021-01-13 15:02:33 +0700

    hehe

--- src/resources/views/user/login.blade.php
+++ src/resources/views/user/login.blade.php
@@ -6,8 +6,9 @@
     <title>Sistem Informasi Manajemen Asset | Unit Pendidikan dan Pelatihan Petrokimia Gresik</title>
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="{{asset('assets/css/themes/lite-purple.min.css')}}" rel="stylesheet">
+    <link rel="icon" href="{{asset('assets/images/1.jpg')}}" sizes="16x16" type="image/png">
 </head>
-<div class="auth-layout-wrap" style="background-image: url(images/bg3.jpg)">
+<div class="auth-layout-wrap" style="background-image: url({{asset('assets/images/bg2.jpg')}})">
     <div class="auth-content">
         <div class="card o-hidden">
             <div class="row">
