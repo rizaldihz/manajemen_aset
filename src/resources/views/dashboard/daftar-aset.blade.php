8f9e559b3758bb543b4037bad77f33d7f4f1b532
Adam Febriansyah <afebriansyah400@gmail.com>
2021-01-13 15:48:22 +0700

    with new logo

--- src/resources/views/dashboard/daftar-aset.blade.php
+++ src/resources/views/dashboard/daftar-aset.blade.php
@@ -47,7 +47,7 @@
             <button class="btn btn-success mb-1" data-toggle="modal" data-target="#modalTambahJenis">Tambah Jenis Asset</button>
         </div> --}}
         {{-- <div class="col-md-12"> --}}
-            <div class="table-responsive ">
+            <div class="table-responsive my-4">
                 <table class="display table table-striped table-bordered" id="asset_table" style="width:100%">
                     <thead>
                         <tr>
